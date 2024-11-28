790b97c8a39d8e5e0a950a9fcfa0e0fc3b1b6e00
Paras <mehtaparas1901@gmail.com>
2024-11-28 23:54:56 +0530

    logic for canvas implemented

---
+++ script.js
@@ -0,0 +1,49 @@
+const canvas = document.querySelector("#draw")
+
+//create context - The context is used for drawing on the canvas. 
+const ctx = canvas.getContext('2d')    //'2d' specifies the type of rendering context, enabling 2-d drawing.
+ctx.strokeStyle = '#FFD700'            //Defines the color used for strokes 
+ctx.lineJoin = 'round'                 //Specifies how two lines join at their "endpoints"
+ctx.lineCap = 'round'                  //Specifies the "shape" of the end of a line 
+
+let isDrawing = false;   // Track drawing state(initialy not drawing)
+let lastX = 0;           // Store the last mouse X position
+let lastY = 0;           // Store the last mouse Y position
+
+//When mousedown then we start drawing
+canvas.addEventListener("mousedown", (e)=>{
+    isDrawing = true 
+    //update the lastX and lastY
+    lastX = e.offsetX
+    lastY = e.offsetY 
+})
+
+//Now Draw while moving the mouse
+canvas.addEventListener("mousemove", (e) =>{
+    if(!isDrawing) return   //stop drawing func when mouse is not down
+    
+    ctx.beginPath()
+    ctx.moveTo(lastX, lastY)         //start from
+    ctx.lineTo(e.offsetX, e.offsetY) //go to
+    ctx.stroke()
+    
+    //update the lastX and lastY
+    lastX = e.offsetX
+    lastY = e.offsetY
+})
+
+//Stop drawing when mouseup and mouseout
+canvas.addEventListener("mouseup", () => isDrawing=false)       
+canvas.addEventListener("mouseout",() => isDrawing=false)     
+
+
+  
+//   canvas.addEventListener("mousemove", (e) => {
+//     if (!isDrawing) return; // Stop if not drawing
+//     ctx.beginPath();
+//     ctx.moveTo(lastX, lastY); // Move to the last position
+//     ctx.lineTo(e.offsetX, e.offsetY); // Draw to the current position
+//     ctx.stroke();
+//     [lastX, lastY] = [e.offsetX, e.offsetY]; // Update last position
+//   });
+  
