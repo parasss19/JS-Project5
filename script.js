21d495724f3dfb097e6d7d9dd969026b9bbe5b13
Paras <mehtaparas1901@gmail.com>
2024-11-29 22:47:42 +0530

    reset and erase button implemented

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("#draw")            //select canvas
 const magicBtn = document.querySelector('#magic-button')  //select magic button
 const baseColorPicker = document.querySelector("#base");  // Select color input
+const eraseBtn = document.querySelector("#erase-button")
+const resetBtn = document.querySelector("#reset-button")
 
 
 //create context - The context is used for drawing on the canvas. 
@@ -16,6 +18,7 @@ let lastX = 0;           // Store the last mouse X position
 let lastY = 0;           // Store the last mouse Y position
 let hue = 0;             // track hue (initialy 0)
 let isMagicMode = false  //track whether magic btn enabled or not
+let isEraseMode = false  //track erase btn
 
 
 //When mousedown then we start drawing
@@ -34,18 +37,23 @@ canvas.addEventListener("mousemove", (e) =>{
     ctx.beginPath()
     ctx.moveTo(lastX, lastY)         //start from
     ctx.lineTo(e.offsetX, e.offsetY) //go to
-    ctx.stroke()
+    ctx.stroke();
     //update the lastX and lastY
     lastX = e.offsetX
     lastY = e.offsetY
 
-    //Change strokeStyle based on mode
+    //Change strokeStyle based on whether isMagicMode true or false
     if(isMagicMode == true){
        ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
        hue++
     }else{
        ctx.strokeStyle = baseColorPicker.value  //Use the selected color
     }
+
+    //Modify strokestyle on mousemove respect erase mode
+    ctx.strokeStyle = isEraseMode 
+            ? (canvas.style.backgroundColor || "#52525B")
+            : (isMagicMode ? `hsl(${hue}, 100%, 50%)` : baseColorPicker.value)
 })
 
 //Stop drawing when mouseup and mouseout
@@ -56,15 +64,30 @@ canvas.addEventListener("mouseout",() => isDrawing=false)
 
 //Magic Button Functionality - Here we use hsl() to make magic stroke
 magicBtn.addEventListener("click", () =>{
-    // isMagicMode = !isMagicMode   
-    if(isMagicMode){
-        isMagicMode = false; // Turn on magic mode
+    isMagicMode = !isMagicMode     //toggle isMagicMode 
+    if(isMagicMode == true){
         magicBtn.textContent = "Stop magic"
     }else{
-        isMagicMode = true; // Turn off magic mode
         magicBtn.textContent = "Magic Button🪄"
     }
 })
 
 
-//
+//Erase button functionality
+eraseBtn.addEventListener("click", ()=>{
+    if(isEraseMode){
+        isEraseMode = false
+        eraseBtn.textContent = "Erase";
+        ctx.strokeStyle = baseColorPicker.value
+    }else{
+        isEraseMode = true
+        eraseBtn.textContent = "Stop Erasing";
+        ctx.strokeStyle = canvas.style.backgroundColor || "#52525B";  //Set to canvas background color  (#52525B = bg-zinc-600)
+    }
+})
+
+
+//Reset button Functionality
+resetBtn.addEventListener("click", ()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //Clear the entire canvas
+})
