ec689cf7e9d1d295f76f0df604148a07efb90606
Paras <mehtaparas1901@gmail.com>
2024-11-29 21:28:11 +0530

    new layout implemented

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      //custom font
+      fontFamily: {
+        "custom-font" : ["Poppins"]
+      }
+    },
   },
   plugins: [],
 }
